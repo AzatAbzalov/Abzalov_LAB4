08ab03706049ae129c254df7b4496538c908b8a8
User <User@LENOVOX250>
2023-03-21 00:42:48 +0300

    Добавьте файлы проекта.

---
+++ DArray.cs
@@ -0,0 +1,97 @@
+﻿
+public class DArray
+{
+    private int capacity;
+    private double[] Arr;
+    public int Size { get; private set; }
+    public int Capacity { get { return capacity; } }
+
+    public DArray()
+    {
+        Arr = Array.Empty<double>();
+        capacity = 0;
+        Size = 0;
+    }
+    public DArray(int capacity)
+    {
+        this.capacity = capacity;
+        Arr = new double[capacity];
+        Size = 0;
+    }
+    public DArray(double[] arr)
+    {
+        capacity = arr.Length*2;
+        Size = arr.Length;
+        Arr = new double[capacity];
+        arr.CopyTo(Arr, 0);
+    } 
+    public double ReadElem(int index) 
+    { return Arr[index]; }
+    public void WriteElem(int i, double value) { Arr[i] = value; }
+    public override string ToString()
+    {
+        string array = "";
+        for (int i = 0; i < Size; i++) { array = array + Arr[i] + " "; }
+        return array;
+    }
+    private void CapacityPlus()
+    {
+        if (Size == capacity)
+        {
+            double[] temp = new double[capacity];
+            Arr.CopyTo(temp,0);
+            if (Size != 0) capacity *= 2;
+            else capacity = 1;
+            Arr = new double[capacity];
+            temp.CopyTo(Arr,0);
+
+        }
+    }
+
+    public void CapacityMinus() 
+    {
+        if (Size == capacity) 
+        {
+            capacity /= 2;
+            Arr = new double[capacity]; 
+            double[] temp = new double[capacity];
+            Array.Copy(Arr, temp, capacity);
+            temp.CopyTo(Arr, 0);
+            
+        }
+       
+    }
+   public void AddElemEnd(double value)
+    {
+        CapacityPlus();
+        Arr[Size++] = value;
+    }
+    public void RemoveElemEnd()
+    {
+        Size--;
+        CapacityMinus();
+    }
+    public void AddInd(double elem, int index)
+    { CapacityPlus();
+        for (int i = Size++; i > index; i--)
+            Arr[i] = Arr[i - 1];
+        Arr[index] = elem;
+    }
+    public void RemoveInd(int index)
+    {
+        for (int i = index; i < Size - 1; i++)
+            Arr[i] = Arr[i + 1];
+        Size--;
+        CapacityMinus();
+    }
+    public int MaxInd()
+    {
+        int index = 0;
+        for (int i = 1; i < Size; i++)
+        {
+            if (Arr[i] > Arr[index]) index = i;
+        }
+        return index;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿DArray a = new();
+Console.WriteLine(a);
+Console.WriteLine("Выделенная память = " + a.Capacity);
+Console.WriteLine("Размер массива = " + a.Size);
+
+DArray b = new(5);
+Console.WriteLine(b);
+Console.WriteLine("Выделенная память = " + b.Capacity);
+Console.WriteLine("Размер массива = " + b.Size);
+
+DArray c = new(new double[] { 8, -4, 2.12, -6.7, -20 });
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+int ind = 1;
+Console.WriteLine($"Элемент массива C с индексом {ind} = " + c.ReadElem(1));
+c.WriteElem(ind,100.3);
+Console.WriteLine($"Массив С с измененным элементом по индексу {ind}: " + c);
+
+double elem = 0;
+c.AddElemEnd(elem);
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+c.RemoveElemEnd();
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+c.RemoveElemEnd();
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+c.AddInd(20, 2);
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+c.RemoveInd(2);
+Console.WriteLine("Массив: " + c);
+Console.WriteLine("Выделенная память = " + c.Capacity);
+Console.WriteLine("Размер массива = " + c.Size);
+
+Console.WriteLine("Индекс макс элемента = " + c.MaxInd());
